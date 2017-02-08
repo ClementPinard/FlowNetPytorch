d20ca79dbc4f46e407dcd1b2013644ba0f2c8b09
Clément Pinard <clement.pinard@parrot.com>
2017-02-08 12:27:56 +0100

    fix typo

--- flow_transforms.py
+++ flow_transforms.py
@@ -32,7 +32,7 @@ class ArrayToTensor(object):
     """Converts a numpy.ndarray (H x W x C) to a torch.FloatTensor of shape (C x H x W)."""
     def __call__(self, array):
         assert(isinstance(array, np.ndarray))
-        array = np.transpose(array, (1, 2, 0))
+        array = np.transpose(array, (2, 0, 1))
         # handle numpy array
         tensor = torch.from_numpy(array)
         # put it from HWC to CHW format
