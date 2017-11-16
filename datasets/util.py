542c6cf4e6af5e9166ae4fdd76dae95a7df27f69
Simon FAUCHARD <simon.fauchard@parrot.com>
2017-11-16 18:46:25 +0100

    fix tab indent bug

--- datasets/util.py
+++ datasets/util.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def split2list(images, split, default_split=0.9):
-	if isinstance(split, str):
+    if isinstance(split, str):
         with open(split) as f:
             split_values = [x.strip() == '1' for x in f.readlines()]
         assert(len(images) == len(split_values))
