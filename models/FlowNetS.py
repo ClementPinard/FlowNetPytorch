bbaf9028c1945cd6cba2b8582e77391dc9936aac
Clément Pinard <clement.pinard@parrot.com>
2017-11-29 12:07:35 +0100

    make FlowNet compatible with non divisible by 64 images

--- models/FlowNetS.py
+++ models/FlowNetS.py
@@ -32,6 +32,13 @@ def deconv(in_planes, out_planes):
     )
 
 
+def crop_like(input, target):
+    if input.size()[2:] == target.size()[2:]:
+        return input
+    else:
+        return input[:, :, :target.size(2), :target.size(3)]
+
+
 class FlowNetS(nn.Module):
     expansion = 1
 
@@ -84,23 +91,23 @@ class FlowNetS(nn.Module):
         out_conv6 = self.conv6_1(self.conv6(out_conv5))
 
         flow6       = self.predict_flow6(out_conv6)
-        flow6_up    = self.upsampled_flow6_to_5(flow6)
-        out_deconv5 = self.deconv5(out_conv6)
+        flow6_up    = crop_like(self.upsampled_flow6_to_5(flow6), out_conv5)
+        out_deconv5 = crop_like(self.deconv5(out_conv6), out_conv5)
 
         concat5 = torch.cat((out_conv5,out_deconv5,flow6_up),1)
         flow5       = self.predict_flow5(concat5)
-        flow5_up    = self.upsampled_flow5_to_4(flow5)
-        out_deconv4 = self.deconv4(concat5)
+        flow5_up    = crop_like(self.upsampled_flow5_to_4(flow5), out_conv4)
+        out_deconv4 = crop_like(self.deconv4(concat5), out_conv4)
 
         concat4 = torch.cat((out_conv4,out_deconv4,flow5_up),1)
         flow4       = self.predict_flow4(concat4)
-        flow4_up    = self.upsampled_flow4_to_3(flow4)
-        out_deconv3 = self.deconv3(concat4)
+        flow4_up    = crop_like(self.upsampled_flow4_to_3(flow4), out_conv3)
+        out_deconv3 = crop_like(self.deconv3(concat4), out_conv3)
 
         concat3 = torch.cat((out_conv3,out_deconv3,flow4_up),1)
         flow3       = self.predict_flow3(concat3)
-        flow3_up    = self.upsampled_flow3_to_2(flow3)
-        out_deconv2 = self.deconv2(concat3)
+        flow3_up    = crop_like(self.upsampled_flow3_to_2(flow3), out_conv2)
+        out_deconv2 = crop_like(self.deconv2(concat3), out_conv2)
 
         concat2 = torch.cat((out_conv2,out_deconv2,flow3_up),1)
         flow2 = self.predict_flow2(concat2)
